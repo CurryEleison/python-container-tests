70b8f037c04e01e748e25271b29eaf716c90acf4
Thomas Petersen <petersen@temp.dk>
2022-01-12 15:24:56 +0100

    Fixed pytest errors

--- slim-testing/tests/test_maths.py
+++ slim-testing/tests/test_maths.py
@@ -3,7 +3,7 @@ from myapp.basic_recursions import factorial, factorial_np
 
 class TestMaths:
     def test_factorial(self):
-        assert factorial(5) == 123
+        assert factorial(5) == 120
 
     def test_factorial_np(self):
-        assert factorial_np(5) == 121
+        assert factorial_np(5) == 120
